b5784ac5f87755f3a5294374328e65baea29307d
Theodore Abitbol <106872220+Teddy1792@users.noreply.github.com>
2023-01-23 15:50:15 +0100

    validity check v1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalClosingBtn = document.querySelectorAll(".close")
+const modalClosingBtn = document.querySelectorAll(".close");
+const modalSubmitBtn = document.querySelectorAll("btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,3 +31,58 @@ function closeModal() {
 }
 
 
+// const needed for the checkModalForm function
+const modalFirst = document.getElementById("first");
+const modalLast = document.getElementById("last");
+const modalEmail = document.getElementById("email");
+const modalBirthdate = document.getElementById("birthdate");
+const modalQuantity = document.getElementById("quantity");
+const modalLocation = document.getElementsByName("location");
+const modalCheckbox1 = document.getElementById("checkbox1");
+
+
+
+//check modal form validity
+function checkFormValidity() {
+  if (
+    modalFirst.value >= 2
+    /* && modalLast.value >= 2
+    && modalEmail.value == /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    && modalBirthdate.value == /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/
+    && Number.isInteger(modalQuantity.value) == true
+    && modalLocation.checked == true
+    && modalCheckbox1.checked == true */
+  ){
+    modalSubmitBtn.removeAttribute('disabled');
+  }
+}
+
+//launch checkFormValidity on field changes and enable button if valid
+
+modalFirst.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+
+/*
+
+modalLast.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+
+modalEmail.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+modalBirthdate.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+modalQuantity.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+modalLocation.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+modalCheckbox1.addEventListener('change', (event) => {
+  checkFormValidity();
+});
+
+*/
