59f4aa2f86abc742bb3b77cb098f4db0b0ff4b41
Theodore Abitbol <106872220+Teddy1792@users.noreply.github.com>
2023-01-24 18:09:37 +0100

    validity check working

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,6 +30,7 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+;
 
 // const needed for the checkFormValidity function
 const modalFirst = document.getElementById("first");
@@ -38,18 +39,17 @@ const modalEmail = document.getElementById("email");
 const modalBirthdate = document.getElementById("birthdate");
 const modalQuantity = document.getElementById("quantity");
 const modalCheckbox1 = document.getElementById("checkbox1");
-
-
+const modalLocation = document.getElementsByName("location");
 
 //check form validity function
-function checkFormValidity() {            //Shouldn't I pass arguments here? The debugger says that "TypeError: modalFirst is undefined" when I do...
+function checkFormValidity() {            //Shouldn't I pass arguments here? The debugger says that "TypeError: modalFirst is undefined" when I do... and works when I don't
   if (
     modalFirst.value.length >= 2 
     && modalLast.value.length >= 2
     && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(modalEmail.value)
-    && (/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/).test(modalBirthdate.value)      //Why is the value stored as YYYY-MM-DD?
+    && (/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/).test(modalBirthdate.value)      //Why is the value stored as YYYY-MM-DD? Is it a standard?
     && (/^\d+$/).test(modalQuantity.value)
-    //&& document.querySelector('input[name="location"]:checked')
+    && checkOneRadioChecked(modalLocation)
     && modalCheckbox1.checked == true 
   ){
     modalSubmitBtn.removeAttribute('disabled', 'disabled');
@@ -62,7 +62,18 @@ function checkFormValidity() {            //Shouldn't I pass arguments here? The
 //launch checkFormValidity on field changes and enable button if valid
 [modalFirst, modalLast, modalEmail, modalBirthdate, modalQuantity, modalCheckbox1].forEach(field => {
   field.addEventListener('change', (e) => {
-    console.log("it's launching!");
     checkFormValidity();
   });
 });
+
+//function to check if radio button has been checked
+function checkOneRadioChecked(modalLocation) {
+  let isSomeOneChecked = false;
+  document.querySelectorAll('input[name="location"]').forEach(input => {
+    if (input.checked) {
+        isSomeOneChecked = true;
+    }
+  })
+
+  return isSomeOneChecked;
+}
