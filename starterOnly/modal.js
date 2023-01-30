6130ddf22d721e0228699fd8f6d605a6dfd61081
Theodore Abitbol <106872220+Teddy1792@users.noreply.github.com>
2023-01-30 17:58:55 +0100

    trial for validity check

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -50,7 +50,7 @@ function checkFormValidity() {            //Shouldn't I pass arguments here? The
     && (/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/).test(modalBirthdate.value)      //Why is the value stored as YYYY-MM-DD? Is it a standard?
     && (/^\d+$/).test(modalQuantity.value)
     && checkOneRadioChecked(modalLocation)
-    && modalCheckbox1.checked == true 
+    && modalCheckbox1.checked 
   ){
     modalSubmitBtn.removeAttribute('disabled', 'disabled');
   }
@@ -59,9 +59,67 @@ function checkFormValidity() {            //Shouldn't I pass arguments here? The
   }
 }
 
+//check field validity function
+function checkFieldValidity() {
+if (modalFirst.value.length < 2){
+  console.log("condition started");
+  document.querySelector(".errorFirst").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  console.log("add HMTL");
+  document.querySelector(".errorFirst").setAttribute("display", "inline");
+  console.log("add display");
+}
+else {
+  document.querySelector(".errorFirst").setAttribute("display", "none");
+}
+
+if(modalLast.value.length < 2){
+  document.querySelector(".errorFirst").setAttribute("display", "inline");
+  document.querySelector(".errorLast").innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+}
+else {
+  document.querySelector(".errorFirst").setAttribute("display", "none");
+}
+
+if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(modalEmail.value)){
+  document.querySelector(".errorEmail").innerHTML = "Veuillez entrer une adresse mail valide.";
+}
+else {
+  document.querySelector(".errorFirst").innerHTML = "";
+}
+
+if(!(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/).test(modalBirthdate.value)){
+  document.querySelector(".errorBirthdate").innerHTML = "Vous devez entrer votre date de naissance.";
+}
+else {
+  document.querySelector(".errorFirst").innerHTML = "";
+}
+
+if(!(/^\d+$/).test(modalQuantity.value)){
+  document.querySelector(".errorQuantity").innerHTML = "Vous devez choisir un nombre de tournoi auquel vous avez participé";
+}
+else {
+  document.querySelector(".errorFirst").innerHTML = "";
+}
+
+if(!checkOneRadioChecked(modalLocation)){
+  document.querySelector(".errorLocation").innerHTML = "Vous devez choisir une option.";
+}
+else {
+  document.querySelector(".errorFirst").innerHTML = "";
+}
+
+if(!modalCheckbox1.checked){
+  document.querySelector(".errorCheckbox").innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+}
+else {
+  document.querySelector(".errorFirst").innerHTML = "";
+}
+}
+
 //launch checkFormValidity on field changes and enable button if valid
 [modalFirst, modalLast, modalEmail, modalBirthdate, modalQuantity, modalCheckbox1].forEach(field => {
-  field.addEventListener('change', (e) => {
+  field.addEventListener('change', (e) => {         //is there a way to pass to the checkFieldValidity function the argument that 'change' was triggered with?
+    checkFieldValidity();
     checkFormValidity();
   });
 });
@@ -74,6 +132,5 @@ function checkOneRadioChecked(modalLocation) {
         isSomeOneChecked = true;
     }
   })
-
   return isSomeOneChecked;
 }
